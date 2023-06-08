a0ad9e2a1f6ea17918c531ac32c62ba79180810b
sahilsoni21 <sahilsoni2112@gmail.com>
2023-06-08 15:22:41 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 function App() {
   return (
     <div >
-     
+     return (
+   <Fragment>
+      <div>
+         Phone Directory
+      </div>
+      <button>Add</button>
+      <div>
+         <span>Name</span>
+         <span>Phone</span>
+      </div>
+   </Fragment>
+)
     </div>
   );
 }
